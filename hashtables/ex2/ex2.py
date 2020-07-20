253a07839b5f119111cc4d69d159f885927774eb
Carl Padilla <carlpadilla@protonmail.com>
2020-07-20 17:35:45 -0400

    completed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,23 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Set up tickets hash table
+    table = {}
+    # Set up route list
+    route = [None] * length
 
+    # Iterate through the tickets
+    for ticket in tickets:
+        # Set source as the key, and destination as the value.
+        table[ticket.source] = ticket.destination
+
+    route[0] = table["NONE"]
+    route[1] = table[route[0]]
+
+    # append to the route
+    if length > 2:
+        for i in range(2, length):
+            route[i] = table[route[i-1]]
+
+    # print(route)
     return route
