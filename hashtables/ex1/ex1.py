9a8988d784e760256f630442cd7b95cb27baf786
Carl Padilla <carlpadilla@protonmail.com>
2020-07-20 17:17:52 -0400

    completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,13 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    paired_numbers = {}
+
+    for i in range(0, length):
+        item = limit - weights[i]
+        if item in paired_numbers:
+            if i != paired_numbers[item]:
+                return [i, paired_numbers[item]]
+        paired_numbers[weights[i]] = i
+
     return None
